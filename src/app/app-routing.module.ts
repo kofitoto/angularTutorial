7003f1c29bccf8f1d8c841ac6bb7a62ee7ee77f6
kofitoto <kofidarko1996@gmail.com>
2021-06-08 12:41:15 +0200

    Added navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { MessagesComponent } from './messages/messages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'messages', component: MessagesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
