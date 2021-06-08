5ef1939e75a1330537872b8e5ae55996dd94c8a5
kofitoto <kofidarko1996@gmail.com>
2021-06-08 12:10:49 +0200

    Message service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,8 +11,9 @@ export class HeroesComponent implements OnInit {
   
   heroes :  Hero [] =[];
   selectedHero?: Hero;
-  constructor(private heroService: HeroService) {}//hemos inyectado el servicio
-
+  constructor(private heroService: HeroService,
+     private messageService: MessageService) { }//hemos inyectado el servicio
+                                                // hemos insertado el servicio de mensajeria
 
   ngOnInit(): void {//cuando arranque el componente y lo tengas en pantalla...
     this.getHeroes();
@@ -19,7 +21,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero : Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
+  
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
